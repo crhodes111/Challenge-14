48476db4d63578fadfdef5c0a207e3f73fa3cf6d
Conner Rhodes <connerrhodes.25@gmail.com>
2022-01-10 16:29:48 -0500

    Login not working right

--- controllers/index.js
+++ controllers/index.js
@@ -5,6 +5,9 @@ const homeRoutes = require('./home-routes.js');
 router.use('/', homeRoutes);
 
 const apiRoutes = require('./api');
+const dashboardRoutes = require('./dashboard-routes.js');
+
+router.use('/dashboard', dashboardRoutes);
 
 router.use('/api', apiRoutes);
 
