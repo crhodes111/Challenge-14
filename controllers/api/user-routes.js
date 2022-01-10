0721a792ad93514e6c9d68ea03cd18033c2a3064
Conner Rhodes <connerrhodes.25@gmail.com>
2022-01-10 16:39:32 -0500

    Login not working right

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -142,5 +142,14 @@ router.delete('/:id', (req, res) => {
       res.status(500).json(err);
     });
 });
-
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
+});
 module.exports = router;
